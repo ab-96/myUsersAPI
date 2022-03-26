bbb8d2483b5c67f22e8ac2ed682629224b8f33e2
ab-96 <abdul.123@live.co.uk>
2022-03-26 22:06:11 +0000

    set up project

---
+++ myUsers/src/test/java/com/example/myUsers/MyUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
